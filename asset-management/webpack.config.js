d65871117033b32939c43ee54e0195615f880dbd
Yushan Fernando <yushanwebdev@gmail.com>
2020-07-23 18:26:27 +0530

    Asset Management in official documentation continue

--- asset-management/webpack.config.js
+++ asset-management/webpack.config.js
@@ -7,20 +7,27 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
-     rules: [
-       {
-         test: /\.css$/,
-         use: [
-           'style-loader',
-           'css-loader',
-         ],
-       },
-       {
-         test: /\.(png|svg|jpg|gif)$/,
-         use: [
-           'file-loader',
-         ],
-       },
+   rules: [
+   {
+     test: /\.css$/,
+     use: [
+     'style-loader',
+     'css-loader',
      ],
    },
+   {
+     test: /\.(png|svg|jpg|gif)$/,
+     use: [
+     'file-loader',
+     ],
+   },
+   {
+     test: /\.(woff|woff2|eot|ttf|otf)$/,
+     use: [
+     'file-loader',
+     ],
+     
+   },
+   ],
+ },
 };
